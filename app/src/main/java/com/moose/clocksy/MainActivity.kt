01942eea353bb0a6d1e13f2adf4241e62ea09063
moose <linus.m.muema@gmail.com>
2021-11-04 14:49:40 +0300

    - added the ordinal date

--- app/src/main/java/com/moose/clocksy/MainActivity.kt
+++ app/src/main/java/com/moose/clocksy/MainActivity.kt
@@ -1,23 +1,19 @@
 package com.moose.clocksy
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement.Center
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.moose.clocksy.ui.theme.ClocksyTheme
 import kotlinx.coroutines.delay
-import java.sql.Time
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.time.ExperimentalTime
@@ -51,9 +47,10 @@ class MainActivity : ComponentActivity() {
         LaunchedEffect(0) {
             while (true) {
                 val cal = Calendar.getInstance()
+                val formatted = SimpleDateFormat("MMM dd, yyyy", Locale.US).format(cal.time)
 
                 time = SimpleDateFormat("HH : mm : ss", Locale.ROOT).format(cal.time)
-                date = SimpleDateFormat("MMM dd, yyyy", Locale.US).format(cal.time)
+                date = formatted.replaceRange(4..5, formatted.substring(4,6).toInt().toOrdinal())
                 delay(1000)
             }
         }
@@ -63,6 +60,12 @@ class MainActivity : ComponentActivity() {
             Spacer(modifier = Modifier.height(10.dp))
             Text(date)
         }
+    }
+
+    private fun Int.toOrdinal(): String {
+        val suffix = arrayOf("th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th")
+        val m = this % 100
+        return "$this${suffix[if (m in 4..20) 0 else m % 10]}"
     }
 }
 
