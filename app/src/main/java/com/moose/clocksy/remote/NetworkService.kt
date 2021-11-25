e85ba65821c82ff2297fed129dc50357cabff1e3
moose <linus.m.muema@gmail.com>
2021-11-25 11:32:11 +0300

    - added the room dependencies

--- app/src/main/java/com/moose/clocksy/remote/NetworkService.kt
+++ app/src/main/java/com/moose/clocksy/remote/NetworkService.kt
@@ -10,7 +10,7 @@ import kotlinx.serialization.json.Json as json
 
 object NetworkService {
 
-    private const val api_key = "8cbfa5c61330375553fc18b29dcd7f0d"
+    private const val api_key = "<YOUR_API_KEY_HERE>"
     private const val base_url = "https://api.openweathermap.org/data/2.5/"
 
     private val client = HttpClient{
